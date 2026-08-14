9be2b9a6213d4d703840d457fb477eea720aabe5
lilafna <lilafna224@gmail.com>
2026-08-14 15:03:11 +0000

    Add new agents to agents.js

--- agents/agents.js
+++ agents/agents.js
@@ -1,10 +1,82 @@
 const agents = {
+    "CDV-AG-000": {
+        nom: "YANG LISHENG",
+        fonction: "PDG",
+        photo: "yang.jpeg",
+        statut: "ACTIF",
+        id: "CDV-AG-000",
+        date: "14/08/2026"
+    },
+    "CDV-AG-001": {
+        nom: "ROUGUIATOU DIALLO",
+        fonction: "Responsable Administratif",
+        photo: "rougui.jpeg",
+        statut: "ACTIF",
+        id: "CDV-AG-001",
+        date: "14/08/2026"
+    },
+    "CDV-AG-002": {
+        nom: "FREDERICK CISSE",
+        fonction: "AGENT COMMERCIAL",
+        photo: "frederic.jpeg",
+        statut: "ACTIF",
+        id: "CDV-AG-002",
+        date: "14/08/2026"
+    },
+    "CDV-AG-003": {
+        nom: "AMADOU SADJO BAH",
+        fonction: "AGENT COMMERCIAL",
+        photo: "sadjo.jpeg",
+        statut: "ACTIF",
+        id: "CDV-AG-003",
+        date: "14/08/2026"
+    },
     "CDV-AG-004": {
+        nom: "BOUBACAR BAILO DIALLO",
+        fonction: "AGENT COMMERCIAL",
+        photo: "bailo.jpeg",
+        statut: "ACTIF",
+        id: "CDV-AG-004",
+        date: "14/08/2026"
+    },
+    "CDV-AG-005": {
         nom: "ISMAEL BARRY",
         fonction: "Agent commercial",
         photo: "ismo.jpeg",
         statut: "ACTIF",
-        id: "CDV-AG-004",
+        id: "CDV-AG-005",
+        date: "14/08/2026"
+    },
+    "CDV-AG-006": {
+        nom: "AMARA CAMARA",
+        fonction: "AGENT COMMERCIAL",
+        photo: "amara.pdf",
+        statut: "ACTIF",
+        id: "CDV-AG-006",
+        date: "14/08/2026"
+    },
+    "CDV-AG-007": {
+        nom: "KAMANO BAKARI",
+        fonction: "AGENT COMMERCIAL",
+        photo: "kamano.jpeg",
+        statut: "ACTIF",
+        id: "CDV-AG-007",
+        date: "14/08/2026"
+    },
+    "CDV-AG-008": {
+        nom: "BAH THIERNO MAMADOU",
+        fonction: "AGENT COMMERCIAL",
+        photo: "mamba.jpeg",
+        statut: "ACTIF",
+        id: "CDV-AG-008",
+        date: "14/08/2026"
+    },
+    "CDV-AG-009": {
+        nom: "MAMADOU TORO BAH",
+        fonction: "AGENT COMMERCIAL",
+        photo: "toro.jpeg",
+        statut: "ACTIF",
+        id: "CDV-AG-009",
         date: "14/08/2026"
     }
 };
